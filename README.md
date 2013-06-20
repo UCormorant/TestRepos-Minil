c61c2b5a64bf0d6c45c9ba0a364c7fa24485af4d
U=Cormorant <u@chimata.org>
2013-06-20 23:29:30 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+TestRepos::Minil - It's new $module
+
+# SYNOPSIS
+
+    use TestRepos::Minil;
+
+# DESCRIPTION
+
+TestRepos::Minil is ...
+
+# LICENSE
+
+Copyright (C) U=Cormorant.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+U=Cormorant <u@chimata.org>
---
+++ lib/TestRepos/Minil.pm
@@ -0,0 +1,39 @@
+package TestRepos::Minil;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+TestRepos::Minil - It's new $module
+
+=head1 SYNOPSIS
+
+    use TestRepos::Minil;
+
+=head1 DESCRIPTION
+
+TestRepos::Minil is ...
+
+=head1 LICENSE
+
+Copyright (C) U=Cormorant.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+U=Cormorant E<lt>u@chimata.orgE<gt>
+
+=cut
+
